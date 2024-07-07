4dc5a90ebdd6cbf465cc372620ae18c1a6d939bb
Alfonso Hernandez <a.hernandez@newxel.online>
2024-07-07 22:44:41 +0200

    feat: remove documentation from Consumer secrets

--- frontend/src/views/ConsumerSecrets/ConsumerSecretsPage.tsx
+++ frontend/src/views/ConsumerSecrets/ConsumerSecretsPage.tsx
@@ -12,19 +12,6 @@ export const ConsumerSecretsPage = () => {
           <h2 className="text-3xl font-semibold text-gray-2000">Consumer Secrets</h2>
           <p className="text-bunker-300">Manage your organizational secrets</p>
         </div>
-        <div className="flex w-max justify-center">
-          <Link href="https://infisical.com/docs/documentation/platform/consumer-secrets" passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            <div className="w-max cursor-pointer rounded-md border border-mineshaft-500 bg-mineshaft-600 px-4 py-2 text-mineshaft-200 duration-200 hover:border-primary/40 hover:bg-primary/10 hover:text-white">
-              Documentation{" "}
-              <FontAwesomeIcon
-                icon={faArrowUpRightFromSquare}
-                className="mb-[0.06rem] ml-1 text-xs"
-              />
-            </div>
-            </a>
-          </Link>
-        </div>
       </div>
       <ConsumerSecretsSection />
     </div>
